b0d72c1dd8c53ae11962b7ddba151cf4a2189d58
JModi16 <k5rac70j@students.codeinstitute.net>
2026-03-03 14:41:30 +0000

    Fix storage backend - use conditional import to avoid boto3 requirement in development

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,21 @@
+import os
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
 
+# Only import S3 storage if AWS is being used
+if 'USE_AWS' in os.environ:
+    from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    class StaticStorage(S3Boto3Storage):
+        location = settings.STATICFILES_LOCATION
 
+    class MediaStorage(S3Boto3Storage):
+        location = settings.MEDIAFILES_LOCATION
+else:
+    # During development, use Django's default file storage
+    from django.core.files.storage import FileSystemStorage
 
-class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    class StaticStorage(FileSystemStorage):
+        pass
+
+    class MediaStorage(FileSystemStorage):
+        pass
