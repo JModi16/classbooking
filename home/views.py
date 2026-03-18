4f5c0da4db70e64afdc6cee042c15d61a3043993
JModi16 <k5rac70j@students.codeinstitute.net>
2026-03-18 12:59:38 +0000

    Add contact page route, view, and template

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """A view to return the contact page"""
+    return render(request, 'home/contact.html')
