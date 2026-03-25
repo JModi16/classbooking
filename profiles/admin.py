fbc96c77427f89e17de1de82f2d9383efe401e7d
JModi16 <k5rac70j@students.codeinstitute.net>
2026-03-25 00:47:17 +0000

    Use category dropdown for instructor class type in admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,27 @@
+from django import forms
 from django.contrib import admin
 from .models import UserProfile, Instructor
+from services.models import Category
+
+
+class InstructorAdminForm(forms.ModelForm):
+    class_type = forms.ChoiceField(required=False)
+
+    class Meta:
+        model = Instructor
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        category_choices = [('', '---------')] + [
+            (category.name, category.name) for category in Category.objects.order_by('name')
+        ]
+
+        current_value = self.instance.class_type if self.instance and self.instance.pk else ''
+        if current_value and current_value not in {value for value, _label in category_choices}:
+            category_choices.append((current_value, current_value))
+
+        self.fields['class_type'].choices = category_choices
 
 
 class UserProfileAdmin(admin.ModelAdmin):
@@ -10,6 +32,7 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 
 class InstructorAdmin(admin.ModelAdmin):
+    form = InstructorAdminForm
     list_display = ('get_name', 'class_type', 'is_verified', 'years_experience', 'rating', 'total_reviews', 'is_active')
     list_filter = ('is_verified', 'is_active', 'created_at', 'years_experience')
     search_fields = ('user__first_name', 'user__last_name', 'user__email', 'specialties', 'class_type')
