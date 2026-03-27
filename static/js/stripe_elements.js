f022531e4bfe4c59e866b557abb0878fcc271520
JModi16 <k5rac70j@students.codeinstitute.net>
2026-03-27 23:33:46 +0000

    Fix Stripe checkout initialization and refresh script cache

--- static/js/stripe_elements.js
+++ static/js/stripe_elements.js
@@ -5,11 +5,12 @@
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
+/* global Stripe */
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = stripe(stripePublicKey);
-var elements = stripe.elements();
+var stripeInstance = Stripe(stripePublicKey);
+var elements = stripeInstance.elements();
 var style = {
     base: {
         color: '#000',
@@ -117,7 +118,7 @@ form.addEventListener('submit', function(ev) {
     var url = '/checkout/cache_checkout_data/';
 
     $.post(url, postData).done(function () {
-        stripe.confirmCardPayment(clientSecret, {
+        stripeInstance.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: primaryCardElement,
             }
